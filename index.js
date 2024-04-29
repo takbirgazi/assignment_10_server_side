4621e60a88f9f34cb9929cd65f42665b1165e654
takbirgazi <takbirgazibd@gmail.com>
2024-04-29 07:45:37 +0600

    Allow Access

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000
 app.use(express.json())
 app.use(cors())
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader("Access-Control-Allow-Origin", "https://assignment-10-5fcf9.web.app");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
