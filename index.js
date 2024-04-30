2dffef6a5338fda2e75f92bf71f9f8043ca87310
takbirgazi <takbirgazibd@gmail.com>
2024-04-30 21:53:52 +0600

    Add DELETE Rout

--- index.js
+++ index.js
@@ -87,6 +87,12 @@ async function run() {
       const details = await allspots.findOne(findId)
       res.send(details);
     })
+    app.delete('/allspot/:id', async (req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await allspots.deleteOne(query);
+      res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
